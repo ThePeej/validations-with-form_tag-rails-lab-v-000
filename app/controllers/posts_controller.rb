e7a1d7b83d7bbfa750d2898b7f6d17e3c869ba6c
PJ Frias <peejfrias@gmail.com>
2018-02-20 20:21:48 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,14 +30,6 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
-  def edit
-    @post = Post.find(params[:id])
-  end
-
-  def update
-    @post = Post.find(params[:id])
-  end
-
   private
 
   def post_params
