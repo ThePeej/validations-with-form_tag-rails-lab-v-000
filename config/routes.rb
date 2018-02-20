e6069a9bbbe9dd40c874e37b7e02ae9809b05097
PJ Frias <peejfrias@gmail.com>
2018-02-20 20:24:17 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  resources :post
-  resources :author
+  resources :posts
+  resources :authors
 end
